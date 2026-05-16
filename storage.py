ef0c7d41992f48f5303e05bed8216c23b5e614ee
Георгий Баженов <georgy.bazhenov.02@gmail.com>
2026-05-16 14:22:24 +0300

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,33 @@
+import json
+import os
+from datetime import datetime
+
+
+def save_to_file(data: dict) -> None:
+    with open("wearther.json", "w", encoding="UTF-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+    print(f"Данные сохранены в файл wearther.json")
+
+
+def file_hour_3_update() -> bool:
+
+    file_path = "wearther.json"
+    file_exists = os.path.exists(file_path)
+    update_needed = True
+
+    if file_exists:
+        file_time = os.path.getmtime(file_path)
+        now = datetime.now().timestamp()
+        hours_since_update = (now - file_time) / 3600
+        if hours_since_update <= 3:
+            return True
+        else:
+            return False
+
+
+def read_from_file() -> dict | None:
+    if file_hour_3_update():
+        with open("wearther.json", "r", encoding="UTF-8") as file:
+            return json.load(file)
+    else:
+        return None
